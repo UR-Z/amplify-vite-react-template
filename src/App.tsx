50b7c1f3632393d190df82696edf1fd9f029c2c3
Zach Chan <zchan@ur.com>
2024-05-30 13:38:26 -0400

    pa website

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
+import { useEffect, useState } from "react";
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
-import React from "react";
-import {useSpeech} from "reach-text-to-speech";
+const client = generateClient<Schema>();
 
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/todos">Todos</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
 
-export default function App() {
-  const {
-    Text, // Component that returns the modified text property
-    speechStatus, // String that stores current speech status
-    isInQueue, // Boolean that stores whether a speech utterance is either being spoken or present in queue
-    start, // Function to start the speech or put it in queue
-    pause, // Function to pause the speech
-    stop, // Function to stop the speech or remove it from queue
-  } = useSpeech({ text: "This library is awesome!" });
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/todos" element={<TodosPage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function LandingPage() {
+  return (
+    <main>
+      <h1>Welcome to HELLO WORLD!</h1>
+      <p>Discover the power of HELLO WORLD and unleash your potential.</p>
+      <button>Get Started</button>
+    </main>
+  );
+}
+
+function TodosPage() {
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  useEffect(() => {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }, []);
+
+  function createTodo() {
+    client.models.Todo.create({ content: window.prompt("Todo content") });
+  }
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", rowGap: "1rem" }}>
-      <Text />
-      <div style={{ display: "flex", columnGap: "0.5rem" }}>
-        {speechStatus !== "started" ? <button onClick={start}>Start</button> : <button onClick={pause}>Pause</button>}
-        <button onClick={stop}>Stop</button>
-      </div>
-    </div>
+    <main>
+      <h1>My todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+function AboutPage() {
+  return (
+    <main>
+      <h1>About Us</h1>
+      <p>We are a team dedicated to bringing you the best HELLO WORLD experience.</p>
+    </main>
   );
 }
 
